abef4eb27dc4966a270b991ab6b26528f8b2e992
dargonza <67393597+dargonza@users.noreply.github.com>
2021-08-16 01:16:27 -0400

    Agregando actividdes
    Actividades de los días 5 y 6 de la semana 8.

---
+++ e_5_8/e_5_8.rb
@@ -0,0 +1,6 @@
+# Dario Gonzalez - Evidencia Dia 1 Semana 8
+print 'Ingrese su nombre: '
+
+nombre = gets.chomp
+
+puts "Hola, #{nombre}!!!"
---
+++ e_6_8/e_6_8.rb
@@ -0,0 +1,8 @@
+# Dario Gonzalez - Evidencia Dia 2 Semana 8
+print 'Ingrese el nombre: '
+nombre = gets.chomp
+
+print 'Ingrese el apellido: '
+apellido = gets.chomp
+
+puts "Hola a todos, yo me llamo #{nombre} #{apellido}!!!"
