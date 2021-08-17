e0dc1a293a47f5e6b0dc9db7ba044eabb25dff45
dargonza <67393597+dargonza@users.noreply.github.com>
2021-08-17 00:26:30 -0400

    Create e_1_9.rb
    Actividad de la evidencia

---
+++ Dia 1 Semana 9/e_1_9.rb
@@ -0,0 +1,34 @@
+# Darío González - Evidencia Día 1 Semana 9
+
+print 'Ingrese el primer operador: '
+operador1 = gets.to_i
+
+print 'Ingrese el segundo operador: '
+operador2 = gets.to_i
+
+print 'Ingrese la operación a efectuar: '
+operacion  = gets.chomp
+
+resultado = 0
+estado = 1
+
+operacion.downcase 
+
+if (operacion == 'suma')
+    resultado = operador1 + operador2    
+elsif (operacion == 'resta')
+    resultado = operador1 - operador2    
+elsif (operacion == 'multiplicacion')
+    resultado = operador1 * operador2    
+elsif (operacion == 'division')
+    resultado = operador1 / operador2    
+else
+    estado = 0 
+end    
+
+
+if (estado == 1)
+    print "El resultado de la '#{operacion}' es igual a #{resultado}."
+else 
+    print "La operación '#{operacion}' no existe."
+end
