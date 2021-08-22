9b3894da33024aea5ae6433ff46be3e52b0e49ca
dargonza <67393597+dargonza@users.noreply.github.com>
2021-08-21 22:22:32 -0400

    Update e_2_9.rb
    Formateo de codigo

--- Dia 2 Semana 9/e_2_9.rb
+++ Dia 2 Semana 9/e_2_9.rb
@@ -1,2 +1,18 @@
-# Dario González - Evidencia Día 2 Semana 9.
+# Darío González - Evidencia Dia 2 Semana 9
 
+# Valor inicial
+print 'Ingrese el primer múmero: '
+first = gets.to_i
+
+# Valor final
+print 'Ingrese el segundo múmero: '
+last = gets.to_i
+
+# calculo
+(first..last).each do |num|
+  if num.even?
+    # Impresion resultado
+    print "#{num}\t"
+  end
+end
+print "\n"
