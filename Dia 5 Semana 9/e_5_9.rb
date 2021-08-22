d8c7a20137ea006429b3bfdce15260ff9f70890c
dargonza <67393597+dargonza@users.noreply.github.com>
2021-08-21 22:23:23 -0400

    Create e_5_9.rb
    Evidencia completada

---
+++ Dia 5 Semana 9/e_5_9.rb
@@ -0,0 +1,17 @@
+print 'Ingrese una oración: ' # captura la oración desde la terminal
+oracion = gets.chomp.to_s
+palabras = oracion.split
+
+
+
+print "\n\n\n------------------------------------------------\n"
+print " Darío González - Evidencia Día 5 Semana 9\n"
+print "------------------------------------------------\n"
+puts  "Oración: #{oracion}"
+print "------------------------------------------------\n"
+acronimo = ''
+palabras.each do |palabra|
+  acronimo.concat(palabra[0].upcase)
+end
+puts "Acronimo: #{acronimo}"
+print "------------------------------------------------\n\n\n"
