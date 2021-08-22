790062c8e102f4fbb5ab2f2812e582f5dfd25c98
dargonza <67393597+dargonza@users.noreply.github.com>
2021-08-21 22:22:21 -0400

    Update e_1_9.rb
    Formateo de codigo

--- Dia 1 Semana 9/e_1_9.rb
+++ Dia 1 Semana 9/e_1_9.rb
@@ -7,28 +7,27 @@ print 'Ingrese el segundo operador: '
 operador2 = gets.to_i
 
 print 'Ingrese la operación (suma, resta, multiplicacion o division) a efectuar: '
-operacion  = gets.chomp
+operacion = gets.chomp
 
 resultado = 0
 estado = 1
 
-operacion.downcase 
+operacion.downcase
 
-if (operacion == 'suma')
-    resultado = operador1 + operador2    
-elsif (operacion == 'resta')
-    resultado = operador1 - operador2    
-elsif (operacion == 'multiplicacion')
-    resultado = operador1 * operador2    
-elsif (operacion == 'division')
-    resultado = operador1 / operador2    
+if operacion == 'suma'
+  resultado = operador1 + operador2
+elsif operacion == 'resta'
+  resultado = operador1 - operador2
+elsif operacion == 'multiplicacion'
+  resultado = operador1 * operador2
+elsif operacion == 'division'
+  resultado = operador1 / operador2
 else
-    estado = 0 
-end    
+  estado = 0
+end
 
-
-if (estado == 1)
-    print "El resultado de la '#{operacion}' es igual a #{resultado}."
-else 
-    print "La operación '#{operacion}' no existe."
+if estado == 1
+  print "El resultado de la '#{operacion}' es igual a #{resultado}."
+else
+  print "La operación '#{operacion}' no existe."
 end
